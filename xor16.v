29a52ae10dc4c6c6511795273c749c1d9ff92ebb
Tachyon01 <pulkitgoel234@gmail.com>
2020-10-07 01:05:23 +0530

    Added xor module

---
+++ xor16.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module xor16_1(
+    output out,
+    input wire [15:0] in
+    );
+
+//    assign out = in[15]^in[14]^in[13]^in[12]^in[11]^in[10]^in[9]^in[8]^in[7]^in[6]^in[5]^in[4]^in[3]^in[2]^in[1]^in[0];
+    xor(out, in[15], in[14], in[13], in[12], in[11], in[10], in[9], in[8], in[7], in[6], in[5], in[4], in[3], in[2], in[1], in[0]);
+
+endmodule
