1471191f8402c79d532feaa417815a4164f5e1e2
gsahil02 <52864776+gsahil02@users.noreply.github.com>
2020-11-09 01:17:16 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,21 +1,23 @@
 `timescale 1ns / 1ps
 
 module tb_trng();
-reg [15:0] A;
-reg clk;
-wire Out;
-smfro_rng SMFRO(A, clk, Out);
+wire [15:0] Out, A, B, temp;
+reg put_seed, clk;
+smfro_rng SMFRO(Out, A, B, temp, put_seed, clk);
 
 initial
 begin
-clk=0;
-forever #5 clk=~clk;
+    clk=0;
+    forever #0.05 clk=~clk;
 end
 
 initial
 begin
-    //A=16'b1011101000101010;
-    #100 $finish;
+    put_seed = 1'b1;
+    #0.07 put_seed = 1'b0;
 end
+
+always@(*)
+    $display("%d",Out);
 
 endmodule
